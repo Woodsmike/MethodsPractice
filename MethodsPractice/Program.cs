c73332cd59e47031bcdd0e59078caaf69c608b18
Woodsmike <Woodsmike@users.noreply.github.com>
2017-07-11 20:41:07 -0400

    last Method problems

--- MethodsPractice/Program.cs
+++ MethodsPractice/Program.cs
@@ -76,51 +76,130 @@ namespace MethodsPractice
 
             Console.WriteLine( "Your monthly wage is {0}" ,WageCalculator(hoursWorked, hourlyWage));
             */
-            Console.WriteLine("What is your first name?");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("What is your last name?");
-            string lastName = Console.ReadLine();
-            fullName(firstName, lastName);
+            //Console.WriteLine("What is your first name?");
+            //string firstName = Console.ReadLine();
+            //Console.WriteLine("What is your last name?");
+            //string lastName = Console.ReadLine();
+            //fullName(firstName, lastName);
 
-            BankAccount(120000, 119000, 35);
-            Console.WriteLine("Your current balance is: {0}", BankAccount(120000, 119000, 35));
+
+            //Console.WriteLine("Your current balance is: {0}", BankAccount(120000, 119000, 35));
+
+
+            //Create a method called FavoriteFood
+            //It should take 2 string parameters.  One representing a name,
+            //and the other representing a favorite food
+
+            //the return type should be void
+            //the method should concatenate.
+            /*public static void FavoriteFood(string name, string foodName)
+            {
+                Console.WriteLine(name + " " + foodName);
+            }*/
+            //Create a method called RetirementCalculator
+            //It should take an int as a parameter representing the user's age
+            //The method should calculate how many more years until the user 
+            //retires using 65 as the age of retirement.
+            //the return type should be void.
+            //Once it calculates the user's retirement age, it should print
+            //"The user will retire in X years" where X represents the value that 
+            //was calculated
+            /*public static void RetirementCalculator(int userAge)
+            {
+                int retirementAge = 65 - userAge;
+                Console.WriteLine("The user will retire in {0} years", retirementAge);
+            }        public static double WageCalculator(double hoursWorkedWeekly, double hourlyWage)
+            {
+                double monthlyWage = (hoursWorkedWeekly * 52 * hourlyWage) / 12;
+                return monthlyWage;
+            }*/
+            /*public static double BankAccount(double amount, double bills ,double lateCharge)
+            {
+                double totalAmount = amount - bills - lateCharge;
+                return totalAmount;
+            }
+            public static void fullName(string firstName, string lastName)
+            {
+                Console.WriteLine("Customer's full name is: {0} {1}", firstName, lastName);
+            }*/
+            //public static void SayHello(string firstName, string lastName)
+            //{            
+            //    Console.WriteLine("Hello " + firstName + lastName);
+            //}
+            //public static void SayGoodbye(string firstName, string lastName)
+            //{
+            //    Console.WriteLine("Goodbye " + firstName + lastName);
+            //}
+
+
+            //problem 3 homework  
+            //sayHello("John");
+
+            //problem 4 homework   sum of an array
+            /*int[] numberInArray = { 4, 5, 10, 12, 23 };
+            Sum(numberInArray);*/
+
+            //problem 5 homework    double an int
+            /*Console.WriteLine("Please enter a number to see what it is doubled.");
+            int numberToDouble = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your number doubled is " + DoubleIt(numberToDouble));*/
+
+            //problem 6 homework    sum of all digits in an int
+            /*            int num;
+            Console.WriteLine("Please enter a number greater than 10.");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine(SumDigits(num));*/
+
+            //problem 7 homework  find array.Max   
+            int[] data = { 87, 90, 23 };
+            Console.WriteLine(FindMaximum(data));
 
         }
-        //Create a method called FavoriteFood
-        //It should take 2 string parameters.  One representing a name,
-        //and the other representing a favorite food
 
-        //the return type should be void
-        //the method should concatenate.
-        /*public static void FavoriteFood(string name, string foodName)
+        //problem 7 homework
+        public static int FindMaximum(int[] data)
         {
-            Console.WriteLine(name + " " + foodName);
+            int max = data.Max();
+            return max;
+        }
+
+
+        //problem 6 homework
+        /*public static int SumDigits(int num)
+        {
+            int sum = 0;
+            
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum;
         }*/
-        //Create a method called RetirementCalculator
-        //It should take an int as a parameter representing the user's age
-        //The method should calculate how many more years until the user 
-        //retires using 65 as the age of retirement.
-        //the return type should be void.
-        //Once it calculates the user's retirement age, it should print
-        //"The user will retire in X years" where X represents the value that 
-        //was calculated
-        /*public static void RetirementCalculator(int userAge)
-        {
-            int retirementAge = 65 - userAge;
-            Console.WriteLine("The user will retire in {0} years", retirementAge);
-        }        public static double WageCalculator(double hoursWorkedWeekly, double hourlyWage)
-        {
-            double monthlyWage = (hoursWorkedWeekly * 52 * hourlyWage) / 12;
-            return monthlyWage;
+
+        //problem 5 homework
+        /*public static int DoubleIt(int numberToDouble)
+        {            
+            int doubleIt = numberToDouble * 2;
+            return doubleIt;
         }*/
-        public static double BankAccount(double amount, double bills ,double lateCharge)
+
+        //problem 4 homework    
+        /*static public void Sum(int[]myarray)
         {
-            double totalAmount = amount - bills - lateCharge;
-            return totalAmount;
-        }
-        public static void fullName(string firstName, string lastName)
+            int sum = 0;
+            for(int i = 0; i < myarray.Length; i++)
+            {
+                sum += myarray[i];
+            }
+            Console.WriteLine(sum);
+        }*/
+
+        //problem 3 homework
+        /*static public void sayHello(string name)
         {
-            Console.WriteLine("Customer's full name is: {0} {1}", firstName, lastName);
-        }
+            Console.WriteLine("Hello " + name);
+        }*/
     }
 }
